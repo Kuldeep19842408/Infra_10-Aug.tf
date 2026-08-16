c0909ddb596396e02f087af720830ab6cd76792a
Kuldeep <kuldeepkumarrf@gmail.com>
2026-08-16 20:09:04 +0530

    new commmit

--- Modules/azurerm_vm/main.tf
+++ Modules/azurerm_vm/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "nic1" {
-    for_each = var.vms
+  for_each            = var.vms
   name                = each.value.nic_name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
@@ -7,19 +7,19 @@ resource "azurerm_network_interface" "nic1" {
   ip_configuration {
     name                          = "dhondu"
     subnet_id                     = data.azurerm_subnet.k-subnet01[each.key].id
-    public_ip_address_id = data.azurerm_public_ip.k-pip01[each.key].id
+    public_ip_address_id          = data.azurerm_public_ip.k-pip01[each.key].id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "virtual_machine" {
-  for_each            = var.vms
-  name                = each.value.vm_name
- resource_group_name = each.value.resource_group_name
-  location            = each.value.location
-  size                = each.value.vm_size
-  admin_username      = each.value.admin_username
-  admin_password      = each.value.admin_password  
+  for_each                        = var.vms
+  name                            = each.value.vm_name
+  resource_group_name             = each.value.resource_group_name
+  location                        = each.value.location
+  size                            = each.value.vm_size
+  admin_username                  = each.value.admin_username
+  admin_password                  = each.value.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
